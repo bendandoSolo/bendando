02f8f8e24a4d899f5224e4749548538d97f4a777
Daniel <dkburton0@gmail.com>
2021-06-01 11:21:40 +0100

    Changed services icons

--- components/ServiceData.js
+++ components/ServiceData.js
@@ -2,22 +2,22 @@ import React from "react";
 
 export const ServiceData = [
   {
-    icon: "fas fa-database",
+    icon: "far fa-object-ungroup",
     title: "DIGITAL DESIGN SERVICES",
     text: "Whether you have a small website that requires nothing more than a few alteration to a template to stand out or a larger website or application that requires branding we have solution for you.",
   },
   {
-    icon: "fas fa-database",
+    icon: "fas fa-desktop",
     title: "WEBSITE DEVELOPMENT",
     text: "We use modern Javascript frameworks to deliver speed optimized websites vastly improving website load times and SEO without the need for pay for click or advertising. Built it right from the start to stand out from your competitors. We do not use WordPress, Wixs or Squarespace.",
   },
   {
-    icon: "fas fa-database",
+    icon: "fas fa-mobile-alt",
     title: "APP DEVELOPMENT",
     text: "With experience working for Microsoft, Vodaphone, the NHS, PWC and Fintech Startups we have the skills to deliver an ontime on budget solution for whatever your needs. Rather than hire us through an agency, come to us directly and we can deliver you product at a fraction of the price.",
   },
   {
-    icon: "fab fa-adn",
+    icon: "fas fa-search",
     title: "Search Engine Optimiztion",
     text: "By building your website with us the right way, all our clients already are head and shoulders above the competition. SEO is changing all the time but fortunately google keep us posted on these changes. There is no one fix solution for SEO, as your competitors evolve so should you.",
   },
@@ -27,7 +27,7 @@ export const ServiceData = [
     text: "We recommend customizing the Shopify Platform for most of our small clients, however can build you a complete E-Commerce solution should that be required. In addition to credit card payments we allow your customers to pay with one click, android pay and apple pay and Crypto Currency. Never has it been easier for payments to be accepted.",
   },
   {
-    icon: "fab fa-adn",
+    icon: "fas fa-search-location",
     title: "Digital Marketing",
     text: "PPC (pay per click) using google Adwords, combined with a Facebook and Instagram advertising campaigns can take your online presence to the next level. By targeting your customer base, monitoring success and making iterative improvements we can boost your website traffic and customer engagement.",
   },
