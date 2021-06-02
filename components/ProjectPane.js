a3d5df66bcb931c5fd5513a0c53329703951100c
Daniel <dkburton0@gmail.com>
2021-06-02 12:10:30 +0100

    Added mobile styling to project panes

--- components/ProjectPane.js
+++ components/ProjectPane.js
@@ -6,6 +6,10 @@ const ProjectPane = ({ title, type, img, bgColor, url }) => {
     <Link href={url}>
       <div className="project-pane">
         <img className="project-img" src={img} />
+        <div className="panel-overlay">
+          <h2>{title}</h2>
+          <h5 style={{ color: "#00E7B6" }}>{type}</h5>
+        </div>
       </div>
     </Link>
   );
