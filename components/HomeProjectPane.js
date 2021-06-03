aa23fe5c43e025af4934afc30dcbbf5954177bec
dan9b6 <dkburton0@gmail.com>
2021-06-03 23:28:43 +0100

    Changed z index of the navbar component.

--- components/HomeProjectPane.js
+++ components/HomeProjectPane.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef } from "react";
-import Link from "next/link";
+import React, { useEffect, useRef } from 'react';
+import Link from 'next/link';
 
 const HomeProjectPane = ({
   projectName,
@@ -15,25 +15,22 @@ const HomeProjectPane = ({
       <div
         className="desktop-infinite-scroll-panel"
         style={{
-          width: "50vw",
-          heigth: "100px",
+          width: '50vw',
+          heigth: '100px',
           backgroundColor: color,
           top: position.top,
           bottom: position.bottom,
-          position: "absolute",
+          position: 'absolute',
           //   backgroundImage: backgroundImage,
-          backgroundPosition: "center",
-          backgroundSize: "cover",
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
         }}
       >
-        <div style={{ height: "100%", width: "100%", overflow: "hidden" }}>
-          <img
-            src={imageURL}
-            style={{ objectFit: "cover", width: "100%", height: "100%" }}
-          />
+        <div style={{ height: '100%', width: '100%', overflow: 'hidden' }}>
+          <img src={imageURL} style={{ objectFit: 'cover', width: '100%', height: '100%' }} />
           <div className="panel-overlay">
             <h2>{projectName}</h2>
-            <h5 style={{ color: "#00E7B6" }}>{projectType}</h5>
+            <h5 style={{ color: '#00E7B6' }}>{projectType}</h5>
           </div>
         </div>
       </div>
