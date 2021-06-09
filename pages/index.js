165c7b7c17b909b623a86f028bf033ecb172a98d
Daniel <dkburton0@gmail.com>
2021-06-09 14:04:45 +0100

    Amended smooth scroll

--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,57 @@
 // Next
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from "next/head";
+import Link from "next/link";
 
 // Components
-import Navbar from '../components/Navbar';
-import ProjectPane from '../components/ProjectPane';
-import HomeProjectPane from '../components/HomeProjectPane';
+import Navbar from "../components/Navbar";
+import ProjectPane from "../components/ProjectPane";
+import HomeProjectPane from "../components/HomeProjectPane";
 
 // react
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from "react";
 
 const Home = () => {
-  const [panel1Pos, setPanel1Pos] = useState({ top: '0px', bottom: '0px' });
-  const [panel2Pos, setPanel2Pos] = useState({ top: '0px', bottom: '0px' });
-  const [panel3Pos, setPanel3Pos] = useState({ top: '0px', bottom: '0px' });
-  const [panel4Pos, setPanel4Pos] = useState({ top: '0px', bottom: '0px' });
+  const [panel1Pos, setPanel1Pos] = useState({ top: "0px", bottom: "0px" });
+  const [panel2Pos, setPanel2Pos] = useState({ top: "0px", bottom: "0px" });
+  const [panel3Pos, setPanel3Pos] = useState({ top: "0px", bottom: "0px" });
+  const [panel4Pos, setPanel4Pos] = useState({ top: "0px", bottom: "0px" });
 
   //should call onresize
   useEffect(() => {
     function handleResize() {
       setPanel1Pos({
-        top: -1 * Math.max(window.innerHeight / 2) + 'px',
-        bottom: 2 * Math.max(window.innerHeight / 2) + 'px',
+        top: -1 * Math.max(window.innerHeight / 2) + "px",
+        bottom: 2 * Math.max(window.innerHeight / 2) + "px",
       });
       setPanel2Pos({
-        top: '0px',
-        bottom: Math.max(window.innerHeight / 2) + 'px',
+        top: "0px",
+        bottom: Math.max(window.innerHeight / 2) + "px",
       });
       setPanel3Pos({
-        top: Math.max(window.innerHeight / 2) + 'px',
-        bottom: '0px',
+        top: Math.max(window.innerHeight / 2) + "px",
+        bottom: "0px",
       });
       setPanel4Pos({
-        top: Math.max((window.innerHeight / 2) * 2) + 'px',
-        bottom: Math.max(-window.innerHeight / 2) + 'px',
+        top: Math.max((window.innerHeight / 2) * 2) + "px",
+        bottom: Math.max(-window.innerHeight / 2) + "px",
       });
     }
 
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   const mouseWheelEvent = (e) => {
-    let deltaY = e.deltaY;
-    if (e.deltaY > 0) deltaY = 50;
-    else deltaY = -50;
+    // let e.deltaY = e.e.deltaY;
+    // if (e.e.deltaY > 0) e.deltaY = 50;
+    // else e.deltaY = -50;
 
     //infinite scrolling
-    if (deltaY < 0) {
+    if (e.deltaY < 0) {
       if (
         parseInt(panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)) >=
         2.5 * Math.max(window.innerHeight / 2)
@@ -60,19 +60,28 @@ const Home = () => {
           top:
             parseInt(panel1Pos.top.substring(0, panel1Pos.top.length - 2)) +
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)) -
+            parseInt(
+              panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)
+            ) -
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel1Pos({
-          top: parseInt(panel1Pos.top.substring(0, panel1Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel1Pos.top.substring(0, panel1Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
       if (
@@ -83,19 +92,28 @@ const Home = () => {
           top:
             parseInt(panel2Pos.top.substring(0, panel2Pos.top.length - 2)) +
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)) -
+            parseInt(
+              panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)
+            ) -
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel2Pos({
-          top: parseInt(panel2Pos.top.substring(0, panel2Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel2Pos.top.substring(0, panel2Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
       if (
@@ -106,19 +124,28 @@ const Home = () => {
           top:
             parseInt(panel3Pos.top.substring(0, panel3Pos.top.length - 2)) +
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)) -
+            parseInt(
+              panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)
+            ) -
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel3Pos({
-          top: parseInt(panel3Pos.top.substring(0, panel3Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel3Pos.top.substring(0, panel3Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
       if (
@@ -129,19 +156,28 @@ const Home = () => {
           top:
             parseInt(panel4Pos.top.substring(0, panel4Pos.top.length - 2)) +
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)) -
+            parseInt(
+              panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)
+            ) -
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel4Pos({
-          top: parseInt(panel4Pos.top.substring(0, panel4Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel4Pos.top.substring(0, panel4Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
     } else {
@@ -153,19 +189,28 @@ const Home = () => {
           top:
             parseInt(panel1Pos.top.substring(0, panel1Pos.top.length - 2)) -
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)) +
+            parseInt(
+              panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)
+            ) +
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel1Pos({
-          top: parseInt(panel1Pos.top.substring(0, panel1Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel1Pos.top.substring(0, panel1Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel1Pos.bottom.substring(0, panel1Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
       if (
@@ -176,19 +221,28 @@ const Home = () => {
           top:
             parseInt(panel2Pos.top.substring(0, panel2Pos.top.length - 2)) -
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)) +
+            parseInt(
+              panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)
+            ) +
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel2Pos({
-          top: parseInt(panel2Pos.top.substring(0, panel2Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel2Pos.top.substring(0, panel2Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel2Pos.bottom.substring(0, panel2Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
 
@@ -200,19 +254,28 @@ const Home = () => {
           top:
             parseInt(panel3Pos.top.substring(0, panel3Pos.top.length - 2)) -
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)) +
+            parseInt(
+              panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)
+            ) +
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel3Pos({
-          top: parseInt(panel3Pos.top.substring(0, panel3Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel3Pos.top.substring(0, panel3Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel3Pos.bottom.substring(0, panel3Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
 
@@ -224,19 +287,28 @@ const Home = () => {
           top:
             parseInt(panel4Pos.top.substring(0, panel4Pos.top.length - 2)) -
             Math.max((window.innerHeight / 2) * 4) +
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)) +
+            parseInt(
+              panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)
+            ) +
             Math.max((window.innerHeight / 2) * 4) -
-            deltaY +
-            'px',
+            e.deltaY +
+            "px",
         });
       } else {
         setPanel4Pos({
-          top: parseInt(panel4Pos.top.substring(0, panel4Pos.top.length - 2)) + deltaY + 'px',
+          top:
+            parseInt(panel4Pos.top.substring(0, panel4Pos.top.length - 2)) +
+            e.deltaY +
+            "px",
           bottom:
-            parseInt(panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)) - deltaY + 'px',
+            parseInt(
+              panel4Pos.bottom.substring(0, panel4Pos.bottom.length - 2)
+            ) -
+            e.deltaY +
+            "px",
         });
       }
     }
@@ -267,14 +339,17 @@ const Home = () => {
                 <a href="#ourprojects">
                   <i
                     className="fas fa-mouse scroll-point"
-                    style={{ color: '#00E7B6', fontSize: '2rem' }}
+                    style={{ color: "#00E7B6", fontSize: "2rem" }}
                   ></i>
                 </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="col-md-6 testing-project-display" style={{ overflow: 'hidden', zIndex: 2 }}>
+        <div
+          className="col-md-6 testing-project-display"
+          style={{ overflow: "hidden", zIndex: 2 }}
+        >
           <HomeProjectPane
             projectName="Malcolm Wall"
             projectType="Website - Branding - Media Feeds"
@@ -330,7 +405,7 @@ const Home = () => {
             img="images/screenshots/2.jpg"
             bgColor="tenants-hub"
             url="/tenants-hub"
-            style={{ height: '45vh' }}
+            style={{ height: "45vh" }}
           />
           <ProjectPane
             title="Win A Flat"
@@ -338,7 +413,7 @@ const Home = () => {
             img="images/screenshots/1.jpg"
             bgColor="winaflat"
             url="/win-a-flat"
-            style={{ height: '45vh' }}
+            style={{ height: "45vh" }}
           />
           <ProjectPane
             title="Green Acre"
@@ -346,7 +421,7 @@ const Home = () => {
             img="images/screenshots/3.jpg"
             bgColor="green-acre"
             url="/green-acre"
-            style={{ height: '45vh' }}
+            style={{ height: "45vh" }}
           />
           <ProjectPane
             title="Malcolm Wall"
@@ -354,7 +429,7 @@ const Home = () => {
             img="images/screenshots/4.jpg"
             bgColor="malcolm-wall"
             url="/malcolm-wall"
-            style={{ height: '45vh' }}
+            style={{ height: "45vh" }}
           />
         </div>
       </div>
