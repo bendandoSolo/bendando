d53eb1eb44562587a373b97e37508f945e690bbe
ben roberts <bendando@hotmail.co.uk>
2021-06-09 16:45:41 +0100

    fixed service page spelling and punctuation

--- components/ServiceData.js
+++ components/ServiceData.js
@@ -5,7 +5,7 @@ export const ServiceData = [
     index: 1,
     icon: "far fa-object-ungroup",
     title: "DIGITAL DESIGN SERVICES",
-    text: "Whether you have a small website that requires nothing more than a few alteration to a template to stand out or a larger website or application that requires branding we have solution for you.",
+    text: "Whether you have a simple website that requires nothing more than a few alterations to a template to stand out or a larger website or application that requires branding we have solution for you.",
   },
   {
     index: 2,
@@ -17,7 +17,7 @@ export const ServiceData = [
     index: 3,
     icon: "fas fa-mobile-alt",
     title: "APP DEVELOPMENT",
-    text: "With experience working for Microsoft, Vodaphone, the NHS, PWC and Fintech Startups we have the skills to deliver an ontime on budget solution for whatever your needs. Rather than hire us through an agency, come to us directly and we can deliver you product at a fraction of the price.",
+    text: "With experience working for Microsoft, Vodaphone, the NHS, PWC and Fintech startups we have the skills to deliver an on time on budget solution for whatever your needs. Rather than hire us through an agency, come to us directly and we can deliver your product at a fraction of the price.",
   },
   {
     index: 4,
@@ -35,7 +35,7 @@ export const ServiceData = [
     index: 6,
     icon: "fas fa-search-location",
     title: "Digital Marketing",
-    text: "PPC (pay per click) using google Adwords, combined with a Facebook and Instagram advertising campaigns can take your online presence to the next level. By targeting your customer base, monitoring success and making iterative improvements we can boost your website traffic and customer engagement.",
+    text: "PPC (pay per click) using Google Ads, combined with a Facebook and Instagram advertising campaigns can take your online presence to the next level. By targeting your customer base, monitoring success and making iterative improvements we can boost your website traffic and customer engagement.",
   },
   {
     index: 7,
@@ -47,6 +47,6 @@ export const ServiceData = [
     index: 8,
     icon: "fas fa-database",
     title: "DATA COLLECTION",
-    text: "We build GDPR compliant databases or integrate with your own to collect whatever information is required from your users. Recently we ran a lottery competition with Uri Gellar to win a flat. Whatever you requirements we can provide a cost effective solution.",
+    text: "We build GDPR compliant databases or integrate with your own to collect whatever information is required from your users. Recently we ran a lottery competition with Uri Gellar to win a flat. Whatever your requirements we can provide a cost effective solution.",
   },
 ];
