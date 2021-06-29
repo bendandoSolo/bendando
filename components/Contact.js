11f25a82c33900fe7cd29281a99c008848ccd50d
Daniel <dkburton0@gmail.com>
2021-06-29 12:51:45 +0100

    Amended dev branch

--- components/Contact.js
+++ components/Contact.js
@@ -1,69 +1,62 @@
-import React, { useEffect } from "react";
+import { useState } from "react";
 
 const Contact = () => {
-  useEffect(() => {
-    let menu = document.getElementById("menu");
-    let prompt = document.getElementById("prompt");
+  const [hover, setHover] = useState(false);
+  const [click, setClick] = useState(false);
+  const [clickCount, setClickCount] = useState(0);
 
-    let hovered = false;
-    let clicked = false;
+  function clickPrompt() {
+    setClickCount(1);
+    setClick(!click);
+    console.log(click);
+  }
 
-    menu.addEventListener("mouseover", () => {
-      hovered = true;
-      if (hovered) {
-        prompt.style.cssText =
-          "animation: prompt-hover 0.6s; animation-fill-mode: both;";
-        form.style.cssText =
-          "animation: fade-in 0.8s; animation-fill-mode: both; animation-delay: 0.8s; display: block;";
-      }
-    });
-
-    menu.addEventListener("click", () => {
-      hovered = false;
-      prompt.style.cssText =
-        "animation: prompt-hover-out 0.8s; animation-fill-mode: both; animation-delay: 0.2s;";
-      form.style.cssText =
-        "animation: fade-out 0.8s; animation-fill-mode: both; ";
-    });
-
-    return () => {};
-  }, []);
+  function hoverPrompt() {
+    setHover(true);
+    if (click) {
+      setTimeout(() => {
+        setHover(false);
+      }, 2000);
+    }
+  }
 
   return (
     <>
-      <i id="menu" className="fas fa-envelope"></i>
-      <div id="prompt">
-        <div id="form">
-          <h3 className="my-4" id="prompt-title">
-            Contact Us
-          </h3>
-          <form>
-            <div className="form-outline my-4">
-              <input type="text" id="form4Example1" className="form-control" />
-              <label className="form-label" htmlFor="form4Example1">
-                Hi, my name is...
-              </label>
-            </div>
-            <div className="form-outline mb-4">
-              <input type="email" id="form4Example2" className="form-control" />
-              <label className="form-label" htmlFor="form4Example2">
-                My email is...
-              </label>
-            </div>
-            <div className="form-outline mb-4">
-              <textarea
-                className="form-control"
-                id="form4Example3"
-                rows="4"
-              ></textarea>
-              <label className="form-label" htmlFor="form4Example3">
-                I wanted to know...
-              </label>
-            </div>
-            <button type="submit" className="btn global-btn">
-              Send
-            </button>
-          </form>
+      <div
+        className="prompt-btn"
+        onMouseEnter={hoverPrompt}
+        onClick={clickPrompt}
+      >
+        <i class="fas fa-envelope"></i>
+      </div>
+      <div className={hover ? "prompt active" : "prompt"}>Contact Us</div>
+      <div
+        className={
+          clickCount == 0
+            ? "prompt-form"
+            : clickCount > 0 && click
+            ? "prompt-form prompt-animate-open"
+            : "prompt-form prompt-animate-close"
+        }
+      >
+        <div
+          className={
+            clickCount > 0 && click
+              ? "prompt-body fade-in"
+              : "prompt-body fade-out"
+          }
+        >
+          <label htmlFor="name">Name:</label>
+          <input type="text" />
+          <label htmlFor="name">Email:</label>
+          <input type="text" />
+          <label htmlFor="name">Number:</label>
+          <input type="text" />
+          <label htmlFor="name">Message:</label>
+          <textarea name="" id="" cols="30" rows="7"></textarea>
+          <button className="global-btn btn" type="submit">
+            Send
+          </button>
         </div>
       </div>
     </>
